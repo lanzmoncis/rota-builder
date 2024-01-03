e235107dec3e0e318d97bca3badb11faf4af39b3
lanzmoncis <lanzmoncis@gmail.com>
2024-01-03 10:25:29 +0000

    Update layout for the app in the root page for the sidebar and the main component dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,7 @@
 import Sidebar from "@/components/sidebar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div>
-      <Sidebar />
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 };
 
 export default DashboardLayout;
