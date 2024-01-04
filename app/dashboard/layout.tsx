fe2b96208f5e049cbe0f9ac6146dfb0cdfa40adf
lanzmoncis <lanzmoncis@gmail.com>
2024-01-04 13:35:33 +0000

    Update file

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import Sidebar from "@/components/sidebar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  console.log("I'M HERE!!");
+
   return <div>{children}</div>;
 };
 
