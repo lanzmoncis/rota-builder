95f852764b6f498a270af868bb504b31dd1216de
lanzmoncis <lanzmoncis@gmail.com>
2024-01-07 23:01:53 +0000

    Add handleDateClick that popup a modal for the employees

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,18 +3,24 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 const DashboardPage = () => {
+  function handleDateClick() {
+    alert("DATE HAS BEEN CLICK");
+  }
   return (
     <div>
       <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin]}
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
         headerToolbar={{
           left: "prev,next today",
           center: "title",
           right: "dayGridMonth,timeGridWeek",
         }}
+        dateClick={handleDateClick}
+        editable={true}
       />
     </div>
   );
