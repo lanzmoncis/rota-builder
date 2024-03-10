08cdc20b3250fe4d53a9dde3a1228ba30fb63da0
lanzmoncis <lanzmoncis@gmail.com>
2024-03-10 21:45:56 +0000

    setup kinde authentication

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,17 @@
-import WeeklyCalendar from "../dashboard/components/weekly-calendar";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+import WeeklyCalendar from "../dashboard/components/weekly-calendar";
 
 const DashboardPage = async () => {
+  const { isAuthenticated } = getKindeServerSession();
+
+  const isLoggedIn = await isAuthenticated();
+
+  if (!isLoggedIn) {
+    redirect("/api/auth/login");
+  }
   const employees = await db.employee.findMany({
     include: {
       shifts: true,
