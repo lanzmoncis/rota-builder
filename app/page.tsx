ce3c0b402e4a530ea96040c193f051606bbed519
lanzmoncis <lanzmoncis@gmail.com>
2024-08-03 01:28:33 +0100

    update landing page and add logo image

--- app/page.tsx
+++ app/page.tsx
@@ -9,65 +9,81 @@ import NextLogo from "../public/next.svg";
 import VercelLogo from "../public/vercel.svg";
 import TailwindLogo from "../public/tailwindcss-logotype.svg";
 import PrismaLogo from "../public/Prisma-DarkLogo.svg";
+import ShiftyLogo from "../public/logo-base-256x256.png";
 
 export default function Home() {
   return (
-    <>
-      <div className="absolute inset-0 -z-10 h-screen w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-300 opacity-20 blur-[100px]"></div>
-      </div>
-
-      <div className="h-screen w-full flex flex-col justify-center items-center">
-        <div className="md:max-w-2xl flex flex-col items-center justify-center text-center px-12 grow">
-          <h1 className="text-8xl md:text-9xl font-medium text-transparent bg-clip-text bg-gradient-to-br from-green-300 via-green-400 to-green-600">
+    <div className="px-4 md:px-10 lg:px-36 py-11 flex flex-col md:items-center h-screen overflow-hidden">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#1AB366_100%)]"></div>
+      <div className="mx-auto flex w-full justify-between">
+        <div className="flex gap-2 items-center">
+          <Image
+            src={ShiftyLogo}
+            className="w-10 h-10"
+            alt="Vercel Logo"
+            priority
+          />
+          <div className="text-2xl md:text-2xl font-extrabold text-[#1AB366]">
             Shifty
-          </h1>
-          <p className="mt-6 text-xs/loose md:text-sm/loose font-medium text-pretty text-gray-500/70 mb-6">
-            Building rotas should be fast and simple. Our platform makes it
-            effortless to create and manage schedules efficiently, so you can
-            focus on what matters most.
-          </p>
-          <Button
-            size={"lg"}
-            className="text-xs md:text-sm md:px-6 px-4 font-semibold"
-          >
-            <LoginLink>Try shifty for free</LoginLink>
+          </div>
+        </div>
+        <div>
+          <Button className="font-semibold tracking-wide rounded-full px-4 bg-[#1AB366]">
+            <LoginLink>Sign up today</LoginLink>
           </Button>
         </div>
-        <div className="pb-8 fixed bottom-0">
-          <div className="flex flex-col items-center">
-            <span className="font-bold uppercase tracking-tighter text-xs text-gray-400 mb-2">
-              built with
-            </span>
-            <div className="aspect-auto flex flex-row gap-4 md:gap-8">
-              <Image
-                src={NextLogo}
-                className="md:w-20 md:h-10 w-16"
-                alt="Next Logo"
-                priority
-              />
-              <Image
-                src={TailwindLogo}
-                className="md:w-36 md:h-10 w-24"
-                alt="Vercel Logo"
-                priority
-              />
-              <Image
-                src={PrismaLogo}
-                className="md:w-20 md:h-10 w-16"
-                alt="Vercel Logo"
-                priority
-              />
-              <Image
-                src={VercelLogo}
-                className="md:w-20 md:h-10 w-16"
-                alt="Vercel Logo"
-                priority
-              />
-            </div>
+      </div>
+      <div className="grow flex items-center justify-center px-4">
+        <div>
+          <p className="text-5xl md:text-7xl text-slate-900 font-semibold text-center mb-6 max-w-4xl mx-auto">
+            Rota <span className="text-[#1AB366]">made simple</span> for small
+            businesses
+          </p>
+          <p className="md:text-lg text-slate-700 text-center mb-6 max-w-2xl mx-auto text-balance tracking-wide">
+            Building rotas should be fast and simple. Our platform makes it
+            effortless to create and manage schedules efficiently, <br /> so you
+            can focus on what matters most.
+          </p>
+          <div className="text-center">
+            <Button className=" font-semibold tracking-wide rounded-full px-4 bg-[#1AB366]">
+              <LoginLink>Get started today</LoginLink>
+            </Button>
           </div>
         </div>
       </div>
-    </>
+      <div className="w-full">
+        <div className="flex flex-col items-center">
+          <span className="font-bold uppercase tracking-wider text-sm text-slate-700 mb-2">
+            built with
+          </span>
+          <div className="aspect-auto flex flex-row gap-4 md:gap-8">
+            <Image
+              src={NextLogo}
+              className="md:w-[120px] md:h-12 w-16"
+              alt="Next Logo"
+              priority
+            />
+            <Image
+              src={TailwindLogo}
+              className="md:w-[190px] md:h-12 w-24"
+              alt="Vercel Logo"
+              priority
+            />
+            <Image
+              src={PrismaLogo}
+              className="md:w-[110px] md:h-12 w-16"
+              alt="Vercel Logo"
+              priority
+            />
+            <Image
+              src={VercelLogo}
+              className="md:w-[110px] md:h-12 w-16"
+              alt="Vercel Logo"
+              priority
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
